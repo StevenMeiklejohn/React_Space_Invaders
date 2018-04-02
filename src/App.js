4ada02c891d812ca740b9d1196c78dc560e2df26
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2018-04-02 11:45:31 +0100

    Initial checkin of react biolerplate.

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import './App.css';
+const width = 400;
+const height = window.innerHeight/2;
+const ratio = window.devicePixelRatio || 1;
+
+this.state = {
+screen: {
+    width: width,
+    height: height,
+    ratio: ratio
+}};
+
+class App extends Component {
+  constructor() {
+  super();
+
+  this.state = {
+  screen: {
+      width: width,
+      height: height,
+      ratio: ratio
+  }};
+};
+
+
+  render() {
+    return (
+      <div>
+        <canvas ref="canvas"
+          width={ this.state.screen.width * this.state.screen.ratio }
+          height={ this.state.screen.height * this.state.screen.ratio } />
+      </div>
+    );
+  }
+}
+
+export default App;
